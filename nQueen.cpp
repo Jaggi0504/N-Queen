c0c3f149ca400e74887f166aaa959ebce31f5281
Jagdeep_Singh <44519331+Jaggi0504@users.noreply.github.com>
2021-05-15 14:51:12 +0530

    Create nQueen.cpp
    Classic nQueen problem- recursive solution

---
+++ nQueen.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isSafe(int board[][10], int i, int j, int n) {
+	for(int row=i-1;row>=0;row--) {
+		if(board[row][j] == 1) {
+			return false;
+		}
+	}
+	int x = i;
+	int y = j;
+	while(x >= 0 && y >= 0) {
+		if(board[x][y] == 1) {
+			return false;
+		}
+		x--;
+		y--;
+	}
+	x = i;
+	y = j;
+	while(x >= 0 && y < n) {
+		if(board[x][y] == 1) {
+			return false;
+		}
+		x--;
+		y++;
+	}
+	return true;
+}
+
+bool nQueen(int board[][10], int i, int n) {
+	if(i == n) {
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(board[i][j] == 1) {
+					cout<<"Q ";
+				} else {
+					cout<<"_ ";
+				}
+			}
+			cout<<endl;
+		}
+		return true;
+	}
+	for(int j=0;j<n;j++) {
+		if(isSafe(board, i, j, n)) {
+			board[i][j] = 1;
+			bool QueenSafelyPlaced = nQueen(board, i+1, n);
+			if(QueenSafelyPlaced) {
+				return true;
+			}
+			board[i][j] = 0;
+		}
+	}
+	return false;
+}
+
+int main() {
+	int n;
+	cin>>n;
+	int board[10][10] = {0};
+	nQueen(board, 0, n);
+	return 0;
+}
